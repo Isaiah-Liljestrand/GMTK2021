f53172afd7738f1a4496efb953c7c2370b0c55ea
bughunterstudios <bughunterstudios@gmail.com>
2021-06-11 19:44:04 -0600

    New Gitignore. Changed prefabs.

---
+++ GMTK2021_game/Assets/Scripts/Connector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connector : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (collision.GetComponent<Rigidbody2D>())
+            {
+                HingeJoint2D newhinge = transform.parent.gameObject.AddComponent<HingeJoint2D>();
+                newhinge.connectedBody = collision.GetComponent<Rigidbody2D>();
+                newhinge.anchor = transform.localPosition;
+                newhinge.enableCollision = true;
+                this.enabled = false;
+            }
+        }
+    }
+}
