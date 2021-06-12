e8c855879e090c6c7e08f3f91efa8155e7496795
bughunterstudios <bughunterstudios@gmail.com>
2021-06-12 15:21:50 -0600

    Fixed issue with connector.

--- GMTK2021_game/Assets/Scripts/Connector.cs
+++ GMTK2021_game/Assets/Scripts/Connector.cs
@@ -26,6 +26,7 @@ public class Connector : MonoBehaviour
                 newhinge.enableCollision = true;
                 newhinge.breakForce = breakforce;
                 GetComponentInParent<Grabbable>().Touch();
+                GetComponent<Collider2D>().enabled = false;
                 this.enabled = false;
             }
         }
