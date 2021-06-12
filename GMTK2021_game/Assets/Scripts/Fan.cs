47a4082f7c21f0c5412677e777bd4af140c4775b
bughunterstudios <bughunterstudios@gmail.com>
2021-06-12 13:10:44 -0600

    Added lots of art.

--- GMTK2021_game/Assets/Scripts/Fan.cs
+++ GMTK2021_game/Assets/Scripts/Fan.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Fan : MonoBehaviour
 {
     public float force;
+    public float animationspeed;
     private Rigidbody2D rb;
     private Grabbable gb;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         gb = GetComponent<Grabbable>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,8 +26,10 @@ public class Fan : MonoBehaviour
             Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousepos.z = 0;
 
-            Debug.Log(Vector2.MoveTowards(Vector2.zero, mousepos - transform.position, force));
+            anim.speed = Mathf.Clamp(Vector2.Distance(mousepos, transform.position), 0, force) * animationspeed;
             rb.AddForce(Vector2.MoveTowards(Vector2.zero, mousepos - transform.position, force));
         }
+        else
+            anim.speed = 0;
     }
 }
